25211f914bc885f91063c1aed3cb58b55eee2658
DHANUSH S <dhanushsubramaniyam26@gmail.com>
2024-09-01 11:32:10 +0530

    rename age to sno

--- src/Insert.java
+++ src/Insert.java
@@ -1,17 +1,15 @@
-import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.util.Scanner;
 class Insert {
     Scanner scan = new Scanner(System.in);
     void insert(String query, CreateConnection object) throws Exception {
         System.out.print("s_no: ");
-        int age = scan.nextInt();
+        int sno = scan.nextInt();
         System.out.print("name: ");
         String name = scan.next();
         PreparedStatement pp = object.con.prepareStatement(query);
         pp.setString(2, name);
-        pp.setInt(1, age);
+        pp.setInt(1, sno);
         pp.executeUpdate();
     }
 }
